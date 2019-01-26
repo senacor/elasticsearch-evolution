6891c46c58dd528860a8f1a3e7387e5b2f3e33f8
Andreas Keefer (Travis CI) <andreas.keefer@senacor.com>
2019-01-26 18:48:40 +0100

    release testing

---
+++ prepare_release.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+#set project version from maven version
+./mvnw help:evaluate -N -Dexpression=project.version|grep -v '\['
+export PROJECT_VERSION=$(./mvnw help:evaluate -N -Dexpression=project.version|grep -v '\[')
+# check if this is a snapshot version
+export IS_SNAPSHOT=$(if [[ $PROJECT_VERSION == *"-SNAPSHOT" ]]; then echo true; else echo false;fi)
+# Set up git user name and tag this commit
+git config --local user.name "Andreas Keefer (Travis CI)"
+git config --local user.email "andreas.keefer@senacor.com"
+#export TRAVIS_TAG=${PROJECT_VERSION}-$(date --iso-8601=seconds)-$(git log --format=%h -1)
+export TRAVIS_TAG=${PROJECT_VERSION}-$(date +'%Y-%m-%dT%H-%M-%S')-$(git log --format=%h -1)
+git tag $TRAVIS_TAG -m "Release ${PROJECT_VERSION}"
+#debug
+set | grep TRAVIS && set | grep SNAPSHOT && set | grep PROJECT
+if [ "$IS_SNAPSHOT" == "true" ]; then
+    echo "can't release a snapshot version: $PROJECT_VERSION"
+    exit 1;
+fi
