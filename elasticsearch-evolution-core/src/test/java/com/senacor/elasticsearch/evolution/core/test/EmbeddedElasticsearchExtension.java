b25ba973b4b74de40c3c5f5ea07de3b36b75140f
Andreas Keefer <keefer.a@gmail.com>
2019-10-26 19:52:55 +0200

    -   test dependency version updates (testcontainers 1.12.3) -   update test Elasticsearch version from 7.4.0 to 7.4.1

--- elasticsearch-evolution-core/src/test/java/com/senacor/elasticsearch/evolution/core/test/EmbeddedElasticsearchExtension.java
+++ elasticsearch-evolution-core/src/test/java/com/senacor/elasticsearch/evolution/core/test/EmbeddedElasticsearchExtension.java
@@ -37,7 +37,7 @@ public class EmbeddedElasticsearchExtension implements TestInstancePostProcessor
     private static final Logger logger = LoggerFactory.getLogger(EmbeddedElasticsearchExtension.class);
     private static final Namespace NAMESPACE = Namespace.create(ExtensionContext.class);
     private static final Set<String> SUPPORTED_ES_VERSIONS = new HashSet<>(Arrays.asList(
-            "7.4.0",
+            "7.4.1",
             "7.3.2",
             "7.2.1",
             "7.1.1",
