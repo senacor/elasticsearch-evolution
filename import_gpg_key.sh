96043cc293ec863f4f123b77f4fa9c7b22119637
Andreas Keefer <xtermi2@users.noreply.github.com>
2020-11-09 02:49:31 +0100

    CI/CD build - release test with github actions

--- import_gpg_key.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# don't fail this script if a single command fails
-set +e
-echo $GPG_SECRET_KEYS | base64 --decode | $GPG_EXECUTABLE --import
-echo $GPG_OWNERTRUST | base64 --decode | $GPG_EXECUTABLE --import-ownertrust
-set -e
-
-echo imported!
