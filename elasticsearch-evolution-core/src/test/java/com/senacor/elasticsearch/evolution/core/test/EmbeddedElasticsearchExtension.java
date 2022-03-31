f06722ed38e53590f679f1e567bd3f6873eafed1
Andreas Keefer <xtermi2@users.noreply.github.com>
2022-03-31 17:01:52 +0200

    update spring-boot to 2.6.6 and elasticsearch 7.17.1 to 7.17.2

--- elasticsearch-evolution-core/src/test/java/com/senacor/elasticsearch/evolution/core/test/EmbeddedElasticsearchExtension.java
+++ elasticsearch-evolution-core/src/test/java/com/senacor/elasticsearch/evolution/core/test/EmbeddedElasticsearchExtension.java
@@ -35,7 +35,7 @@ public class EmbeddedElasticsearchExtension implements TestInstancePostProcessor
     private static final Logger logger = LoggerFactory.getLogger(EmbeddedElasticsearchExtension.class);
     private static final Namespace NAMESPACE = Namespace.create(ExtensionContext.class);
     private static final SortedSet<String> SUPPORTED_ES_VERSIONS = Collections.unmodifiableSortedSet(new TreeSet<>(Arrays.asList(
-            "7.17.1",
+            "7.17.2",
             "7.16.3",
             "7.15.2",
             "7.14.2",
