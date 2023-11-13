b33af6ec01cd078e09cb0ef6ee5809e2a7a8e92a
Andreas Keefer <xtermi2@users.noreply.github.com>
2023-11-13 22:56:30 +0100

    - update maven wrapper - update spring-boot version to 2.7.17 - added regression tests on JDK 21 - added regression tests for spring boot 3.1 - updated elasticsearch and opensearch regression versions

--- elasticsearch-evolution-core/src/test/java/com/senacor/elasticsearch/evolution/core/test/EmbeddedElasticsearchExtension.java
+++ elasticsearch-evolution-core/src/test/java/com/senacor/elasticsearch/evolution/core/test/EmbeddedElasticsearchExtension.java
@@ -28,6 +28,7 @@ import java.io.IOException;
 import java.util.*;
 import java.util.stream.Stream;
 
+import static com.senacor.elasticsearch.evolution.core.test.EmbeddedElasticsearchExtension.SearchContainer.ofElasticsearch;
 import static com.senacor.elasticsearch.evolution.core.test.EmbeddedElasticsearchExtension.SearchContainer.ofOpensearch;
 import static java.time.Duration.ofMinutes;
 import static org.elasticsearch.client.RequestOptions.DEFAULT;
@@ -43,7 +44,15 @@ public class EmbeddedElasticsearchExtension implements TestInstancePostProcessor
     private static final Logger logger = LoggerFactory.getLogger(EmbeddedElasticsearchExtension.class);
     private static final Namespace NAMESPACE = Namespace.create(ExtensionContext.class);
     private static final SortedSet<SearchContainer> SUPPORTED_SEARCH_VERSIONS = Collections.unmodifiableSortedSet(new TreeSet<>(Arrays.asList(
-            ofOpensearch("1.1.0")
+            ofOpensearch("2.11.0"),
+            ofOpensearch("2.10.0"),
+            ofOpensearch("2.9.0"),
+            ofOpensearch("1.3.12"),
+
+            ofElasticsearch("8.11.1"),
+            ofElasticsearch("8.10.4"),
+            ofElasticsearch("8.9.1"),
+            ofElasticsearch("7.17.15")
     )));
 
     @Override
