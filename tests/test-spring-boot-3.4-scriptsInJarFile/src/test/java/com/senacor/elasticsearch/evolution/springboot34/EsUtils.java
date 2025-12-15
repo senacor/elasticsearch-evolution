bd45ee51af7e91f2b21975c19341776ccf0db0b9
David Otero <david.otero@gbtec.es>
2025-12-15 08:45:13 +0100

    update package name for Spring Boot 3.4 test module

--- tests/test-spring-boot-3.4-scriptsInJarFile/src/test/java/com/senacor/elasticsearch/evolution/springboot34/EsUtils.java
+++ tests/test-spring-boot-3.4-scriptsInJarFile/src/test/java/com/senacor/elasticsearch/evolution/springboot34/EsUtils.java
@@ -1,4 +1,4 @@
-package com.senacor.elasticsearch.evolution.springboot30;
+package com.senacor.elasticsearch.evolution.springboot34;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
